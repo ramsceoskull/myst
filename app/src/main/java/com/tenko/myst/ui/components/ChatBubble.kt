b9d49441d2176c2d5c768eece1cb7b01f55b3307
Ramses Anceno <ramses.social0@gmail.com>
2026-04-18 10:31:03 -0600

    Message bubble to handle chat responses

--- app/src/main/java/com/tenko/myst/ui/components/ChatBubble.kt
+++ app/src/main/java/com/tenko/myst/ui/components/ChatBubble.kt
@@ -1,54 +1,99 @@
 package com.tenko.myst.ui.components
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import coil.compose.AsyncImage
 import com.tenko.myst.R
+import com.tenko.myst.data.model.ChatMessage
+import com.tenko.myst.data.view.AuthViewModel
+import com.tenko.myst.ui.theme.AntiFlashWhite
+import com.tenko.myst.ui.theme.PompAndPower
+import com.tenko.myst.ui.theme.White
 
 @Composable
-fun ChatBubble() {
+fun ChatBubble(message: ChatMessage, navController: NavController, authViewModel: AuthViewModel = viewModel()) {
+    authViewModel.getUser(navController)
+    val user = authViewModel.currentUser
+
+    val alignment = if (message.isUser) Arrangement.End else Arrangement.Start
+
     Row(
         modifier = Modifier.fillMaxWidth(),
-        horizontalArrangement = Arrangement.End
+        verticalAlignment = Alignment.Top,
+        horizontalArrangement = alignment
     ) {
-        Card(
-            shape = RoundedCornerShape(20.dp),
-            colors = CardDefaults.cardColors(
-                containerColor = Color(0xFFE5E5E5)
-            ),
-            modifier = Modifier.widthIn(max = 260.dp)
+        if(!message.isUser) {
+            Image(
+                painter = painterResource(R.drawable.tenko_avatar),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(50.dp)
+                    .clip(CircleShape)
+                    .border(2.dp, PompAndPower, CircleShape)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+        }
+
+        Box(
+            modifier = Modifier
+                .background(
+                    color = if (message.isUser) White else PompAndPower,
+                    shape = RoundedCornerShape(12.dp)
+                )
+                .border(
+                    width = 2.dp,
+                    color = if (message.isUser) Color(0xFFE0E0E0) else PompAndPower,
+                    shape = RoundedCornerShape(12.dp)
+                )
+                .padding(horizontal = 16.dp, vertical = 10.dp)
+                .widthIn(max = 250.dp)
         ) {
             Text(
-                text = "Lorem ipsum dolor sit amet...",
-                modifier = Modifier.padding(16.dp)
+                text = message.text,
+                fontSize = 14.sp,
+                color = if(message.isUser) Color.Black else White
             )
         }
 
-        Spacer(modifier = Modifier.width(8.dp))
-
-        Image(
-            painter = painterResource(R.drawable.mujer0),
-            contentDescription = null,
-            modifier = Modifier
-                .size(40.dp)
-                .clip(CircleShape)
-        )
+        if(message.isUser) {
+            Spacer(modifier = Modifier.width(8.dp))
+            AsyncImage(
+                model = user?.picture,
+                contentDescription = "Profile Photo",
+                modifier = Modifier
+                    .size(50.dp)
+                    .clip(CircleShape)
+                    .border(2.dp, AntiFlashWhite, CircleShape),
+                placeholder = painterResource(R.drawable.profile_picture_placeholder),
+                error = painterResource(R.drawable.profile_picture_placeholder)
+            )
+        }
     }
+
+    Spacer(modifier = Modifier.height(8.dp))
 }
