066149e6420e99f2af37925a653fc667423f8bf9
Ramses Anceno <ramses.social0@gmail.com>
2026-04-18 10:38:17 -0600

    Now able to update individual fields

--- app/src/main/java/com/tenko/myst/ui/screen/UpdateProfileScreen.kt
+++ app/src/main/java/com/tenko/myst/ui/screen/UpdateProfileScreen.kt
@@ -1,55 +1,78 @@
 package com.tenko.myst.ui.screen
 
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.pulltorefresh.PullToRefreshBox
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.core.content.FileProvider
+import androidx.core.net.toUri
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.tenko.myst.data.api.TokenManager
+import com.tenko.myst.data.serializable.UserUpdate
 import com.tenko.myst.data.view.AuthViewModel
 import com.tenko.myst.data.view.ProfilePictureViewModel
 import com.tenko.myst.launcher.rememberCameraLauncher
 import com.tenko.myst.launcher.rememberGalleryLauncher
+import com.tenko.myst.navigation.AppScreens
 import com.tenko.myst.ui.components.AppTopBar
 import com.tenko.myst.ui.components.DeleteAccountRow
 import com.tenko.myst.ui.components.InfoRow
 import com.tenko.myst.ui.components.PhotoActionsSection
+import com.tenko.myst.ui.theme.AntiFlashWhite
+import com.tenko.myst.ui.theme.PompAndPower
+import com.tenko.myst.ui.theme.SweetGrey
 import com.tenko.myst.ui.theme.Tekhelet
 import com.tenko.myst.ui.theme.White
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import java.io.File
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun UpdateProfileScreen(navController: NavController, viewModel: AuthViewModel, tokenManager: TokenManager, profileViewModel: ProfilePictureViewModel) {
+fun UpdateProfileScreen(navController: NavController, tokenManager: TokenManager, viewModel: AuthViewModel = viewModel()) {
     viewModel.getUser(navController)
     val user = viewModel.currentUser
 
+    var newValue by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
     var showDialog by remember { mutableStateOf(false) }
-    var showModal by remember { mutableStateOf(false) }
+    var showInput by remember { mutableStateOf(false) }
+
+    var isRefreshing by remember { mutableStateOf(false) }
+    val scope = rememberCoroutineScope()
 
     val context = LocalContext.current
-    val photoUri by profileViewModel.photoUri.collectAsState()
 
 //    URI temporal para camara
     val tempUri = remember {
@@ -62,12 +85,24 @@ fun UpdateProfileScreen(navController: NavController, viewModel: AuthViewModel, 
     }
 
 //    Launchers
-    val galleryLauncher = rememberGalleryLauncher {
-        profileViewModel.updatePhoto(it)
+    val galleryLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.GetContent()
+    ) { uri : Uri? ->
+        uri?.let {
+            viewModel.updateProfilePicture(it, context)
+        }
+
     }
+   /* val galleryLauncher = rememberGalleryLauncher {
+
+        val updateUserPhoto = UserUpdate(picture = it.toString())
+        viewModel.updateUser(updateUserPhoto)
+    }*/
     val cameraLauncher = rememberCameraLauncher(tempUri) { success ->
-        if(success)
-            profileViewModel.updatePhoto(tempUri)
+        if(success) {
+            val updateUserPhoto = UserUpdate(picture = tempUri.toString())
+            viewModel.updateUser(updateUserPhoto)
+        }
     }
 
     Scaffold(
@@ -85,108 +120,139 @@ fun UpdateProfileScreen(navController: NavController, viewModel: AuthViewModel, 
                 .background(White)
                 .fillMaxSize()
         ) {
-            Column(
-                modifier = Modifier
-                    .padding(16.dp)
-                    .fillMaxSize()
+            PullToRefreshBox(
+                isRefreshing = isRefreshing,
+                onRefresh = {
+                    scope.launch {
+                        isRefreshing = true
+                        delay(2000)
+                        navController.popBackStack()
+                        navController.navigate(AppScreens.UpdateProfileScreen.route)
+                        isRefreshing = false
+                    }
+                },
             ) {
-                PhotoActionsSection(
-                    imageUrl = photoUri?.toString(),
-                    onEditClick = { galleryLauncher.launch("image/*") },
-                    onRemoveClick = { profileViewModel.removePhoto() }
-                )
+                LazyColumn(Modifier.fillMaxSize()) {
+                    item {
 
-                Text(
-                    text = "Tus datos personales",
-                    modifier = Modifier.padding(vertical = 16.dp),
-                    style = MaterialTheme.typography.labelLarge,
-                    color = Tekhelet
-                )
+                        Column(
+                            modifier = Modifier
+                                .padding(16.dp)
+                                .fillMaxSize()
+                        ) {
+                            PhotoActionsSection(
+                                imageUrl = user?.picture?.toUri(),
+                                onEditClick = { galleryLauncher.launch("image/*") },
+                                onRemoveClick = { viewModel.updateUser(UserUpdate(picture = "")) }
+                            )
 
-                InfoRow(
-                    label = "Nombre",
-                    value = user?.name ?: "Jane Doe",
-                    onClick = { /* Lógica para editar nombre */ }
-                )
+                            Text(
+                                text = "Tus datos personales",
+                                modifier = Modifier.padding(vertical = 16.dp),
+                                style = MaterialTheme.typography.labelLarge,
+                                color = Tekhelet
+                            )
 
-                InfoRow(
-                    label = "Correo electrónico",
-                    value = user?.email ?: "tenko@myst.com",
-                    showArrow = false,
-                    onClick = { /* Lógica para editar correo */ }
-                )
+                            InfoRow(
+                                label = "Nombre",
+                                value = user?.name ?: "Jane Doe",
+                                onClick = { showInput = true }
+                            )
 
-                Text(
-                    text = "Otro",
-                    modifier = Modifier.padding(vertical = 16.dp),
-                    style = MaterialTheme.typography.labelLarge,
-                    color = Tekhelet
-                )
-
-                DeleteAccountRow(
-                    label = "Eliminar cuenta",
-                    onClick = { showDialog = true }
-                )
-
-                /*if(showDialog)
-                    Column {
-                        Text("Esta acción no se puede deshacer.\nPor favor, ingresa tu contraseña para confirmar:")
-                        Spacer(modifier = Modifier.height(8.dp))
-                        OutlinedTextField(
-                            value = password,
-                            onValueChange = { password = it },
-                            label = { Text("Contraseña") },
-                            visualTransformation = PasswordVisualTransformation()
-                        )
-                        Row {
-                            TextButton(onClick = { showDialog = false }) {
-                                Text("Cancelar")
+                            if(showInput) {
+                                Column {
+                                    Spacer(modifier = Modifier.height(8.dp))
+                                    OutlinedTextField(
+                                        value = newValue,
+                                        onValueChange = { newValue = it },
+                                        placeholder = { Text("Ingresa el nuevo nombre:") },
+                                        singleLine = true,
+                                        shape = RoundedCornerShape(12.dp),
+                                        colors = OutlinedTextFieldDefaults.colors(
+                                            focusedContainerColor = White,
+                                            unfocusedContainerColor = AntiFlashWhite,
+                                            focusedBorderColor = PompAndPower,
+                                            unfocusedBorderColor = Color.Transparent,
+                                            unfocusedPlaceholderColor = Color.Gray
+                                        ),
+                                        modifier = Modifier.fillMaxWidth()
+                                    )
+                                    Row {
+                                        TextButton(onClick = { showInput = false }) {
+                                            Text("Cancelar", color = PompAndPower)
+                                        }
+                                        TextButton(onClick = {
+                                            viewModel.updateUser(UserUpdate(name = newValue))
+                                            scope.launch {
+                                                isRefreshing = true
+                                                delay(2000)
+                                                navController.popBackStack()
+                                                navController.navigate(AppScreens.UpdateProfileScreen.route)
+                                                isRefreshing = false
+                                            }
+                                            showInput = false
+                                        }) {
+                                            Text("Cambiar nombre", color = Tekhelet)
+                                        }
+                                    }
+                                }
                             }
-                            TextButton(onClick = {
-                                viewModel.deleteUser(password, tokenManager, navController)
-//                                showDialog = false
-                            }) {
-                                Text("Eliminar definitivamente")
-                            }
-                        }
-                    }*/
-            }
 
-            if (showDialog) {
-                AlertDialog(
-                    onDismissRequest = { showDialog = false },
-                    title = { Text("¿Estás segura?") },
-                    text = {
-                        Column {
-                            Text("Esta acción no se puede deshacer.\nPor favor, ingresa tu contraseña para confirmar:")
-                            Spacer(modifier = Modifier.height(8.dp))
-                            OutlinedTextField(
-                                value = password,
-                                onValueChange = { password = it },
-                                label = { Text("Contraseña") },
-                                visualTransformation = PasswordVisualTransformation()
+                            InfoRow(
+                                label = "Correo electrónico",
+                                value = user?.email ?: "tenko@myst.com",
+                                showArrow = false,
+                                onClick = { /* Lógica para editar correo */ }
+                            )
+
+                            Text(
+                                text = "Otro",
+                                modifier = Modifier.padding(vertical = 16.dp),
+                                style = MaterialTheme.typography.labelLarge,
+                                color = Tekhelet
+                            )
+
+                            DeleteAccountRow(
+                                label = "Eliminar cuenta",
+                                onClick = { showDialog = true }
                             )
                         }
-                    },
-                    confirmButton = {
-                        TextButton(onClick = {
-                            println("--- MYST DEBUG: confirmar ---")
-                            if(password.isNotBlank()) {
-                                println("--- MYST DEBUG: if---")
-                                viewModel.deleteUser(password, tokenManager, navController)
-                                println("--- MYST DEBUG: listo ---")
-                                showDialog = false
-                            }
-                        }) {
-                            Text("Eliminar definitivamente")
-                        }
-                    },
-                    dismissButton = {
-                        TextButton(onClick = { showDialog = false }) {
-                            Text("Cancelar")
+
+                        if (showDialog) {
+                            AlertDialog(
+                                onDismissRequest = { showDialog = false },
+                                title = { Text("¿Estás segura?") },
+                                text = {
+                                    Column {
+                                        Text("Esta acción no se puede deshacer.\nPor favor, ingresa tu contraseña para confirmar:")
+                                        Spacer(modifier = Modifier.height(8.dp))
+                                        OutlinedTextField(
+                                            value = password,
+                                            onValueChange = { password = it },
+                                            label = { Text("Contraseña") },
+                                            visualTransformation = PasswordVisualTransformation()
+                                        )
+                                    }
+                                },
+                                confirmButton = {
+                                    TextButton(onClick = {
+                                        if(password.isNotBlank()) {
+                                            viewModel.deleteUser(password, tokenManager, navController)
+                                            showDialog = false
+                                        }
+                                    }) {
+                                        Text("Eliminar definitivamente")
+                                    }
+                                },
+                                dismissButton = {
+                                    TextButton(onClick = { showDialog = false }) {
+                                        Text("Cancelar")
+                                    }
+                                }
+                            )
                         }
                     }
-                )
+                }
             }
         }
     }
