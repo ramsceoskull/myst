31e4220a6e84e23ec7a9ffa66a2edbb71973b798
Ramses Anceno <ramses.social0@gmail.com>
2026-04-28 23:52:08 -0600

    Improved topbar, now including actions

--- app/src/main/java/com/tenko/app/ui/components/AppTopBar.kt
+++ app/src/main/java/com/tenko/app/ui/components/AppTopBar.kt
@@ -1,25 +1,33 @@
-package com.tenko.myst.ui.components
+package com.tenko.app.ui.components
 
 import androidx.compose.animation.AnimatedContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MediumTopAppBar
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
@@ -27,31 +35,38 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.net.toUri
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
-import coil.compose.AsyncImage
-import com.tenko.myst.R
-import com.tenko.myst.data.model.Quote
-import com.tenko.myst.data.view.AuthViewModel
-import com.tenko.myst.data.view.NotificationViewModel
-import com.tenko.myst.navigation.AppScreens
-import com.tenko.myst.ui.theme.AntiFlashWhite
-import com.tenko.myst.ui.theme.Monserrat
-import com.tenko.myst.ui.theme.PompAndPower
-import com.tenko.myst.ui.theme.StarsLove
-import com.tenko.myst.ui.theme.SweetGrey
-import com.tenko.myst.ui.theme.Tekhelet
-import com.tenko.myst.ui.theme.White
+import androidx.wear.compose.material3.ConfirmationDialog
+import androidx.wear.compose.material3.TextButtonDefaults
+import com.tenko.app.R
+import com.tenko.app.data.model.Quote
+import com.tenko.app.data.view.AuthViewModel
+import com.tenko.app.data.view.NotificationViewModel
+import com.tenko.app.navigation.AppScreens
+import com.tenko.app.ui.theme.AntiFlashWhite
+import com.tenko.app.ui.theme.Monserrat
+import com.tenko.app.ui.theme.PompAndPower
+import com.tenko.app.ui.theme.StarsLove
+import com.tenko.app.ui.theme.SweetGrey
+import com.tenko.app.ui.theme.Tekhelet
+import com.tenko.app.ui.theme.White
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 
 val quotes = listOf(
@@ -76,11 +91,11 @@ val quotes = listOf(
 @Composable
 fun AppTopBar(
     title: String,
-    showBackButton: Boolean = true,
     onBackClick: (() -> Unit)? = null,
-    navigationIcon: Int = R.drawable.chevron_left_solid_full,
-    actions: @Composable RowScope.() -> Unit = {}
+    actions: Triple<() -> Unit, Int, String> = Triple({}, 0, "")
 ) {
+    var showDialog by remember { mutableStateOf(false) }
+
     Surface(
         shadowElevation = 4.dp,
         border = BorderStroke(1.dp, AntiFlashWhite)
@@ -93,25 +108,84 @@ fun AppTopBar(
                     fontFamily = StarsLove,
                     fontWeight = FontWeight.SemiBold,
                     fontSize = 32.sp,
-                    modifier = Modifier.padding(top = 8.dp)
+                    modifier = Modifier.offset(y = 4.dp)
                 )
             },
             navigationIcon = {
-                if (showBackButton)
-                    onBackClick?.let {
-                        IconButton(onClick = it) {
-                            Icon(
-                                modifier = Modifier.size(28.dp),
-                                painter = painterResource(navigationIcon),
-                                contentDescription = "Back",
-                                tint = Tekhelet
-                            )
-                        }
+                onBackClick?.let {
+                    IconButton(onClick = it) {
+                        Icon(
+                            modifier = Modifier.size(28.dp),
+                            painter = painterResource(R.drawable.chevron_left_solid_full),
+                            contentDescription = "Back",
+                            tint = Tekhelet
+                        )
                     }
+                }
             },
-            actions = actions,
-            colors = TopAppBarDefaults.topAppBarColors( containerColor = White )
+            actions = {
+                if(actions.second != 0) {
+                    IconButton(onClick = { showDialog = true }) {
+                        Icon(
+                            modifier = Modifier.size(28.dp),
+                            painter = painterResource(id = actions.second),
+                            contentDescription = null,
+                            tint = Tekhelet
+                        )
+                    }
+                }
+            },
+            colors = TopAppBarDefaults.topAppBarColors(containerColor = White),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 8.dp)
         )
+
+        if(showDialog) {
+            AlertDialog(
+                onDismissRequest = { showDialog = false },
+                confirmButton = {
+                    TextButton(
+                        onClick = {
+                            actions.first()
+                            showDialog = false
+                        },
+                        shape = RoundedCornerShape(12.dp),
+                        colors = ButtonDefaults.textButtonColors(
+                            contentColor = White,
+                            containerColor = Tekhelet
+                        ),
+                        content = { Text("Confirmar") }
+                    )
+                },
+                dismissButton = {
+                    TextButton(
+                        onClick = { showDialog = false },
+                        content = { Text("Cancelar", color = SweetGrey) }
+                    )
+                },
+                title = {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.spacedBy(8.dp)
+                    ) {
+                        Icon(
+                            modifier = Modifier.size(24.dp),
+                            painter = painterResource(actions.second),
+                            contentDescription = null,
+                        )
+                        Text("¿Estás segura?")
+                    }
+                },
+                text = {
+                    Text(actions.third)
+                },
+                shape = RoundedCornerShape(12.dp),
+                containerColor = White,
+                titleContentColor = Tekhelet,
+                textContentColor = SweetGrey
+            )
+        }
     }
 }
 
@@ -125,9 +199,18 @@ fun AppTopBar(
     authViewModel: AuthViewModel = viewModel(),
     actions: () -> Unit
 ) {
-    authViewModel.getUser(navController)
-    val hasUnread = notificationViewModel.hasUnread
+    // 1. Obtenemos el usuario del estado del ViewModel (es reactivo)
     val user = authViewModel.currentUser
+
+    // 2. Si por alguna razón el usuario es nulo (ej. refresco manual),
+    // lo pedimos una SOLA VEZ al entrar.
+    LaunchedEffect(Unit) {
+        if (user == null) {
+            authViewModel.getUser(navController)
+        }
+    }
+
+    val hasUnread = notificationViewModel.hasUnread
 
     Surface(
         shadowElevation = 4.dp,
@@ -141,32 +224,17 @@ fun AppTopBar(
                     fontFamily = StarsLove,
                     fontWeight = FontWeight.SemiBold,
                     fontSize = 32.sp,
-                    modifier = Modifier.padding(top = 8.dp, start = 8.dp)
+                    modifier = Modifier.offset(y = 4.dp)
                 )
             },
             navigationIcon = {
                 IconButton(
                     onClick = { navController.navigate(AppScreens.ProfileScreen.route) },
-                    modifier = Modifier
-                        .padding(start = 8.dp)
-                        .size(40.dp)
-                        .clip(CircleShape),
-                    content = {
-                        AsyncImage(
-                            model = user?.picture,
-                            contentDescription = "Profile Photo",
-                            modifier = Modifier
-                                .size(150.dp)
-                                .clip(CircleShape),
-                            contentScale = ContentScale.Crop,
-                            placeholder = painterResource(R.drawable.profile_picture_placeholder),
-                            error = painterResource(R.drawable.profile_picture_placeholder)
-                        )
-                    }
+                    content = { ProfilePicture(user?.picture?.toUri(), 40.dp, false) }
                 )
             },
             actions = {
-                IconButton(onClick = actions, modifier = Modifier.padding(end = 8.dp)) {
+                IconButton(onClick = actions) {
                     if(hasUnread) {
                         Icon(
                             tint = null,
@@ -186,7 +254,10 @@ fun AppTopBar(
             },
             colors = TopAppBarDefaults.topAppBarColors( containerColor = White, scrolledContainerColor = White ),
 
-            scrollBehavior = scrollBehavior
+            scrollBehavior = scrollBehavior,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 8.dp)
         )
     }
 }
