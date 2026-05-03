e89a358ecc1071e8a7fb727dfe492fbce7ea5cab
Ramses Anceno <ramses.social0@gmail.com>
2026-05-03 09:24:15 -0600

    Chat input handler for numeric data

--- app/src/main/java/com/tenko/app/ui/components/ChatInput.kt
+++ app/src/main/java/com/tenko/app/ui/components/ChatInput.kt
@@ -1,5 +1,6 @@
-package com.tenko.myst.ui.components
+package com.tenko.app.ui.components
 
+import android.widget.Toast
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -8,6 +9,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -22,14 +24,30 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
-import com.tenko.myst.R
-import com.tenko.myst.ui.theme.Tekhelet
+import com.tenko.app.R
+import com.tenko.app.ui.theme.SweetGrey
+import com.tenko.app.ui.theme.Tekhelet
 
 @Composable
-fun ChatInput(onSend: (String) -> Unit, modifier: Modifier = Modifier, isNumeric : Boolean = false) {
+fun ChatInput(
+    onSend: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    isNumeric : Boolean = false
+) {
+    val colors = TextFieldDefaults.colors(
+        unfocusedContainerColor = Color.Transparent,
+        focusedContainerColor = Color.Transparent,
+        unfocusedIndicatorColor = Color.Transparent,
+        focusedIndicatorColor = Color.Transparent
+    )
+    val context = LocalContext.current
+
     var text by remember { mutableStateOf("") }
 
     Row(
@@ -51,15 +69,12 @@ fun ChatInput(onSend: (String) -> Unit, modifier: Modifier = Modifier, isNumeric
                         text = newText
                     }
                 },
-                placeholder = { Text("Digite 0 para saltar...") },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                placeholder = { Text("Digite la cantidad...") },
+                supportingText = { if(text.isBlank()) Text("Si no aplica, deje en blanco o escriba 0", color = SweetGrey) },
                 modifier = Modifier.weight(1f),
-                colors = TextFieldDefaults.colors(
-                    unfocusedContainerColor = Color.Transparent,
-                    focusedContainerColor = Color.Transparent,
-                    unfocusedIndicatorColor = Color.Transparent,
-                    focusedIndicatorColor = Color.Transparent
-                ),
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+                singleLine = true,
+                colors = colors
             )
         } else {
             TextField(
@@ -67,12 +82,8 @@ fun ChatInput(onSend: (String) -> Unit, modifier: Modifier = Modifier, isNumeric
                 onValueChange = { text = it },
                 placeholder = { Text("Escribe tu mensaje...") },
                 modifier = Modifier.weight(1f),
-                colors = TextFieldDefaults.colors(
-                    unfocusedContainerColor = Color.Transparent,
-                    focusedContainerColor = Color.Transparent,
-                    unfocusedIndicatorColor = Color.Transparent,
-                    focusedIndicatorColor = Color.Transparent
-                )
+                singleLine = true,
+                colors = colors
             )
         }
 
@@ -80,9 +91,18 @@ fun ChatInput(onSend: (String) -> Unit, modifier: Modifier = Modifier, isNumeric
 
         IconButton(
             onClick = {
-                if (text.isNotBlank()) {
-                    onSend(text)
-                    text = ""
+                when {
+                    text.isNotBlank() -> {
+                        onSend(text.trim())
+                        text = ""
+                    }
+
+                    text.isBlank() -> {
+                        if (!isNumeric)
+                            Toast.makeText(context, "No puedes enviar un mensaje vacío", Toast.LENGTH_SHORT).show()
+                        else
+                            onSend("0")
+                    }
                 }
             },
             content = {
