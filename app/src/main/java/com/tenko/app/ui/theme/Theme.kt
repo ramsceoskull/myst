60f18e4ddc21d101dd078ab50c844363c722400e
Ramses Anceno <ramses.social0@gmail.com>
2026-05-03 09:19:41 -0600

    Theme color scheme updated

--- app/src/main/java/com/tenko/app/ui/theme/Theme.kt
+++ app/src/main/java/com/tenko/app/ui/theme/Theme.kt
@@ -10,16 +10,12 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
-
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Tekhelet,
+    onPrimary = AntiFlashWhite,
+    secondary = PompAndPower,
+    tertiary = RaisinBlack,
+    background = White,
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -34,23 +30,11 @@ private val LightColorScheme = lightColorScheme(
 
 @Composable
 fun MystTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-      dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-        val context = LocalContext.current
-        if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-      }
-      darkTheme -> DarkColorScheme
-      else -> LightColorScheme
-    }
-
     MaterialTheme(
-      colorScheme = colorScheme,
-      typography = Typography,
-      content = content
+        colorScheme = LightColorScheme,
+        typography = Typography,
+        content = content
     )
 }
