4472e7d363faafa2bf30bb873303b4dbf222284b
Ramses Anceno <ramses.social0@gmail.com>
2026-05-07 15:16:18 -0600

    New code version (doctor flip card)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.tenko.app"
         minSdk = 30
         targetSdk = 36
-        versionCode = 6
-        versionName = "1.1.0"
+        versionCode = 7
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
