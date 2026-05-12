6cb1f1b60dd98040622162060c83f14b9e17ea46
Ramses Anceno <ramses.social0@gmail.com>
2026-05-11 18:51:58 -0600

    Card now adapts to different width

--- app/src/main/java/com/tenko/app/ui/components/ActionCard.kt
+++ app/src/main/java/com/tenko/app/ui/components/ActionCard.kt
@@ -1,62 +1,112 @@
 package com.tenko.app.ui.components
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.tenko.app.R
+import com.tenko.app.ui.theme.AntiFlashWhite
 import com.tenko.app.ui.theme.SweetGrey
 import com.tenko.app.ui.theme.Tekhelet
 import com.tenko.app.ui.theme.White
 
 @Composable
 fun ActionCard(
-    icon: ImageVector,
+    icon: Int,
     title: String,
     subtitle: String,
-    onClick : (() -> Unit)? = null,
-    color: Color = Tekhelet,
+    modifier: Modifier = Modifier,
+    onClick: (() -> Unit)? = null
 ) {
     Card(
         onClick = { onClick?.invoke() },
         enabled = onClick != null,
+        shape = RoundedCornerShape(12.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = White,
+            contentColor = Tekhelet,
+            disabledContainerColor = White,
+            disabledContentColor = SweetGrey.copy(alpha = 0.5f)
+        )
     ) {
         Row(
-            horizontalArrangement = Arrangement.Start,
+            modifier = modifier.padding(horizontal = 12.dp, vertical = 16.dp),
             verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier
-                .clip(RoundedCornerShape(12.dp))
-                .background(White)
-                .padding(horizontal = 12.dp, vertical = 16.dp)
-                .width(138.dp)
+            horizontalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterHorizontally)
         ) {
-            Icon(icon, null, tint = color, modifier = Modifier.size(32.dp))
+            Icon(
+                painter = painterResource(icon),
+                contentDescription = title,
+                modifier = Modifier.size(32.dp)
+            )
 
-            Spacer(modifier = Modifier.width(8.dp))
-
-            Column(
-                modifier = Modifier.fillMaxSize()
-            ) {
-                Text(title, color = color, fontSize = 18.sp, fontWeight = FontWeight.Bold)
-                Text(subtitle, fontSize = 14.sp, color = SweetGrey)
+            Column {
+                Text(text = title, fontSize = 18.sp, fontWeight = FontWeight.Bold)
+                Text(text = subtitle, fontSize = 14.sp, color = SweetGrey)
             }
+        }
+    }
+}
+
+@Composable
+fun ActionCard(
+    icon: Int,
+    title: String,
+    onClick: (() -> Unit)? = null
+) {
+    Card(
+        onClick = { onClick?.invoke() },
+        enabled = onClick != null,
+        shape = RoundedCornerShape(12.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = AntiFlashWhite,
+            contentColor = Tekhelet,
+            disabledContainerColor = SweetGrey.copy(alpha = 0.5f),
+            disabledContentColor = SweetGrey.copy(alpha = 0.5f)
+        ),
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(20.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                painter = painterResource(icon),
+                contentDescription = title,
+                modifier = Modifier.size(30.dp)
+            )
+
+            Spacer(modifier = Modifier.width(16.dp))
+
+            Text(
+                text = title,
+                fontSize = 18.sp,
+                modifier = Modifier.weight(1f)
+            )
+
+            Icon(
+                painter = painterResource(R.drawable.chevron_right_solid_full),
+                contentDescription = "Proceed",
+                modifier = Modifier.size(25.dp)
+            )
         }
     }
 }
