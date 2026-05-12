041892f61fa89cbab5e91f2bc4ea69e8a841ffa3
Ramses Anceno <ramses.social0@gmail.com>
2026-05-12 17:55:20 -0600

    New version code (clinical history variable updates)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.tenko.app"
         minSdk = 30
         targetSdk = 36
-        versionCode = 10
-        versionName = "1.2.0"
+        versionCode = 11
+        versionName = "1.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
