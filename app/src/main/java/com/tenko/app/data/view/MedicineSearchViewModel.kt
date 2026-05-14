baeac70febc6b418307e3e338ec8c7d330846b1a
Ramses Anceno <ramses.social0@gmail.com>
2026-05-14 13:52:12 -0600

    Medicine search view model

--- app/src/main/java/com/tenko/app/data/view/MedicineSearchViewModel.kt
+++ app/src/main/java/com/tenko/app/data/view/MedicineSearchViewModel.kt
@@ -2,7 +2,9 @@ package com.tenko.app.data.view
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.tenko.app.data.api.MedicineApi
 import com.tenko.app.data.model.MedicineRepository
 import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -10,11 +12,12 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.launch
 
 @OptIn(FlowPreview::class)
-class MedicineSearchViewModel(application: Application) : AndroidViewModel(application) {
-    private val repository = MedicineRepository(application)
+class MedicineSearchViewModel : ViewModel() {
+    private val api = MedicineApi()
     private val _query = MutableStateFlow("")
     val query = _query.asStateFlow()
 
@@ -28,11 +31,12 @@ class MedicineSearchViewModel(application: Application) : AndroidViewModel(appli
     init {
         viewModelScope.launch {
             _query
-                .debounce(250)
+                .debounce(400)
+                .filter { it.isNotBlank() }
                 .distinctUntilChanged()
                 .collectLatest { query ->
-                    _results.value =
-                        repository.searchMedicines(query)
+                    _results.value = api.searchMedicines(query)
+                    print("API Response Search results for \"$query\": ${_results.value}") // Debug log
                 }
         }
     }
