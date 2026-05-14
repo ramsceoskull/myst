93ddccb316802709e800aa2ff0e8c62e975b2362
Ramses Anceno <ramses.social0@gmail.com>
2026-05-14 13:49:24 -0600

    Prediction serializable

--- app/src/main/java/com/tenko/app/data/serializable/PredictionRange.kt
+++ app/src/main/java/com/tenko/app/data/serializable/PredictionRange.kt
@@ -1,8 +1,18 @@
 package com.tenko.app.data.serializable
+
 import kotlinx.serialization.Serializable
+import java.time.LocalDate
 
 @Serializable
 data class PredictionRange(
-    val start: String,
-    val end: String
+    val predicted_length: Double,
+
+    @Serializable(with = LocalDateSerializer::class)
+    val predicted_next_period: LocalDate,
+
+    @Serializable(with = LocalDateSerializer::class)
+    val earliest_expected_date: LocalDate,
+
+    @Serializable(with = LocalDateSerializer::class)
+    val latest_expected_date: LocalDate
 )
