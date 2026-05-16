7b38f993d165a76a802eeb54bf44be8294aeded7
Ramses Anceno <ramses.social0@gmail.com>
2026-05-15 23:29:01 -0600

    Trend's serializable

--- app/src/main/java/com/tenko/app/data/serializable/Trend.kt
+++ app/src/main/java/com/tenko/app/data/serializable/Trend.kt
@@ -1,11 +1,16 @@
 package com.tenko.app.data.serializable
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 enum class Trend {
+    @SerialName("UP")
     UP,
+    @SerialName("DOWN")
     DOWN,
+    @SerialName("STABLE")
     STABLE,
+    @SerialName("NONE")
     NONE
 }
