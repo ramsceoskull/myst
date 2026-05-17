12407e2cc5354ec4b2b3e8687eaf95fefc288fbc
Ramses Anceno <ramses.social0@gmail.com>
2026-05-17 09:01:23 -0600

    Splash loader and notification permission launcher

--- app/src/main/java/com/tenko/app/ui/screen/MainScreen.kt
+++ app/src/main/java/com/tenko/app/ui/screen/MainScreen.kt
@@ -1,23 +1,21 @@
 package com.tenko.app.ui.screen
 
+import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
-import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.pulltorefresh.PullToRefreshBox
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -25,33 +23,31 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.tenko.app.R
+import com.tenko.app.data.model.DialogButton
+import com.tenko.app.data.model.DialogTitle
 import com.tenko.app.data.model.MedicineStatus
+import com.tenko.app.data.notifications.NotificationPermissionHelper
 import com.tenko.app.data.serializable.ReminderResponse
 import com.tenko.app.data.view.AuthViewModel
 import com.tenko.app.data.view.MedicineViewModel
-import com.tenko.app.data.view.NotificationViewModel
 import com.tenko.app.navigation.AppScreens
-import com.tenko.app.ui.components.FloatingActionButton
+import com.tenko.app.ui.components.AlertDialog
 import com.tenko.app.ui.components.AppTopBar
 import com.tenko.app.ui.components.BottomNavigationBar
 import com.tenko.app.ui.components.EmptyMedicationState
 import com.tenko.app.ui.components.FilterSection
+import com.tenko.app.ui.components.FloatingActionButton
 import com.tenko.app.ui.components.MedicationCard
 import com.tenko.app.ui.components.SuggestionsCard
 import com.tenko.app.ui.theme.BackgroundColor
-import com.tenko.app.ui.theme.SweetGrey
-import com.tenko.app.ui.theme.Tekhelet
 import com.tenko.app.ui.theme.White
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -59,151 +55,127 @@ import com.tenko.app.ui.theme.White
 fun MainScreen(
     navController: NavController,
     authViewModel: AuthViewModel = viewModel(),
-    notificationViewModel: NotificationViewModel,
     medicineViewModel: MedicineViewModel = viewModel()
 ) {
-//    val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
-    LaunchedEffect(Unit) {
-        medicineViewModel.fetchMedicationReminders()
-    }
-    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
+    LaunchedEffect(Unit) { medicineViewModel.fetchMedicationReminders() }
 
+    val context = LocalContext.current
+    val isRefreshing = medicineViewModel.isLoading
     val medicines by medicineViewModel.filteredMedicines.collectAsState()
     val medication by medicineViewModel.medicines.collectAsState()
     val currentFilter by medicineViewModel.filter.collectAsState()
 
-//    var showNotifications by remember { mutableStateOf(false) }
     var medicineToDelete by remember { mutableStateOf<ReminderResponse?>(null) }
-
-    Scaffold(
-        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
-        topBar = {
-            AppTopBar(
-                title = "Myst",
-                navController = navController,
-                scrollBehavior = scrollBehavior,
-                notificationViewModel = notificationViewModel,
-                authViewModel = authViewModel,
-//                actions = { showNotifications = !showNotifications }
-            )
-        },
-        floatingActionButton = {
-            FloatingActionButton(R.drawable.red_and_white_pills, false) {
-                navController.navigate(AppScreens.AddMedicationScreen.route)
-            }
-        },
-        bottomBar = { BottomNavigationBar(navController) },
-        containerColor = BackgroundColor,
-    ) { paddingValues ->
-        Box {
-            LazyColumn(
-                contentPadding = paddingValues,
-                verticalArrangement = Arrangement.spacedBy(16.dp),
-                modifier = Modifier.padding(horizontal = 20.dp, vertical = 30.dp)
-            ) {
-                item { SuggestionsCard(navController) }
-
-                item {
-                    Text(
-                        text = "Control de medicamentos",
-                        fontSize = 20.sp,
-                        fontWeight = FontWeight.SemiBold,
-                        modifier = Modifier.padding(top = 16.dp)
-                    )
-                }
-
-                if (medication.isEmpty())
-                    item { EmptyMedicationState(filter = MedicineStatus.ALL) }
-                else {
-                    item {
-                        FilterSection(viewModel = medicineViewModel)
-
-                        if (medicines.isEmpty())
-                            EmptyMedicationState(filter = currentFilter)
-                    }
-
-                    items(medicines) { medicine ->
-                        MedicationCard(
-                            medicine = medicine,
-                            onTaken = {
-                                medicineViewModel.updateReminderStatus(
-                                    medicine.id_reminder,
-                                    MedicineStatus.TAKEN.ordinal
-                                )
-                            },
-                            onSkipped = {
-                                medicineViewModel.updateReminderStatus(
-                                    medicine.id_reminder,
-                                    MedicineStatus.SKIPPED.ordinal
-                                )
-                            },
-                            onDelete = { medicineToDelete = medicine }
-                            /*onInfo = { navController.navigate("detail/${it.id}") },
-                            onEdit = { navController.navigate("edit/${it.id}") },
-                            onDelete = { medicineToDelete = it }*/
-                        )
-                    }
-                }
-            }
-
-            /*if (showNotifications) {
-                NotificationsOverlay(
-                    padding = paddingValues.calculateTopPadding(),
-                    viewModel = notificationViewModel,
-                    onDismiss = { showNotifications = false },
-                    onSeeAllClick = { navController.navigate(AppScreens.AllNotificationsScreen.route) },
-                    onNotificationClick = { navController.navigate("notification_details_screen/${it.id}") }
-                )
-            }*/
-        }
+    val notificationPermissionLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        if (!isGranted) {
+            Toast.makeText(
+                context,
+                "Necesitamos permiso de notificaciones para usar esta función",
+                Toast.LENGTH_LONG
+            ).show()
+        } else
+            navController.navigate(AppScreens.AddMedicationScreen.route)
     }
 
-    if (medicineToDelete != null) {
-        AlertDialog(
-            onDismissRequest = { medicineToDelete = null },
-            confirmButton = {
-                TextButton(
-                    onClick = {
-                        medicineToDelete?.let { medicineViewModel.deleteReminder(it.id_reminder) }
-                        medicineViewModel.fetchMedicationReminders()
-                        medicineToDelete = null
-                    },
-                    shape = RoundedCornerShape(12.dp),
-                    colors = ButtonDefaults.textButtonColors(
-                        contentColor = White,
-                        containerColor = Tekhelet
-                    ),
-                    content = { Text("Eliminar") }
+    Box(modifier = Modifier.fillMaxSize()) {
+        Scaffold(
+            topBar = {
+                AppTopBar(
+                    title = "Myst",
+                    navController = navController,
+                    authViewModel = authViewModel,
                 )
             },
-            dismissButton = {
-                TextButton(
-                    onClick = { medicineToDelete = null },
-                    content = { Text("Cancelar", color = Color.Gray) }
-                )
-            },
-            title = {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.spacedBy(8.dp)
-                ) {
-                    Icon(
-                        painter = painterResource(R.drawable.trash_solid_full),
-                        contentDescription = "Delete Icon",
-                        modifier = Modifier.size(24.dp)
-                    )
-                    Text("Eliminar medicación")
+            floatingActionButton = {
+                FloatingActionButton(R.drawable.red_and_white_pills, false) {
+                    if (NotificationPermissionHelper.hasNotificationPermission(context))
+                        navController.navigate(AppScreens.AddMedicationScreen.route)
+                    else
+                        NotificationPermissionHelper
+                            .requestNotificationPermission(notificationPermissionLauncher)
                 }
             },
-            text = {
-                Text(
-                    "La siguiente acción eliminará el recordatorio de medicación \"${medicineToDelete?.title}\".\n¿Deseas continuar?",
-                )
-            },
-            shape = RoundedCornerShape(12.dp),
-            containerColor = White,
-            titleContentColor = Tekhelet,
-            textContentColor = SweetGrey
-        )
+            bottomBar = { BottomNavigationBar(navController) },
+            containerColor = BackgroundColor,
+        ) { paddingValues ->
+            PullToRefreshBox(
+                isRefreshing = isRefreshing,
+                onRefresh = { medicineViewModel.fetchMedicationReminders() },
+                modifier = Modifier
+                    .background(White)
+                    .padding(paddingValues)
+            ) {
+                LazyColumn(
+                    verticalArrangement = Arrangement.spacedBy(16.dp),
+                    modifier = Modifier.padding(start = 20.dp, top = 30.dp, end = 20.dp)
+                ) {
+                    item { SuggestionsCard(navController) }
+
+                    item {
+                        Text(
+                            text = "Control de medicamentos",
+                            fontSize = 20.sp,
+                            fontWeight = FontWeight.SemiBold,
+                            modifier = Modifier.padding(top = 16.dp)
+                        )
+                    }
+
+                    if (medication.isEmpty() && !medicineViewModel.isLoading)
+                        item { EmptyMedicationState(filter = MedicineStatus.ALL) }
+                    else {
+                        item {
+                            FilterSection(viewModel = medicineViewModel)
+
+                            if (medicines.isEmpty() && !medicineViewModel.isLoading)
+                                EmptyMedicationState(filter = currentFilter)
+                        }
+
+                        items(medicines) { medicine ->
+                            MedicationCard(
+                                medicine = medicine,
+                                onTaken = {
+                                    medicineViewModel.updateReminderStatus(
+                                        medicine.id_reminder,
+                                        MedicineStatus.TAKEN.ordinal
+                                    )
+                                },
+                                onSkipped = {
+                                    medicineViewModel.updateReminderStatus(
+                                        medicine.id_reminder,
+                                        MedicineStatus.SKIPPED.ordinal
+                                    )
+                                },
+                                onDelete = { medicineToDelete = medicine }
+                                /*onEdit = { navController.navigate("edit/${it.id}") }*/
+                            )
+                        }
+                    }
+
+                    item { Spacer(modifier = Modifier.height(30.dp)) }
+                }
+            }
+        }
+
+        if (medicineToDelete != null)
+            AlertDialog(
+                onDismissRequest = { medicineToDelete = null },
+                title = DialogTitle(R.drawable.trash_solid_full, "¿Eliminar medicación?"),
+                confirmButton = DialogButton("Sí, eliminar") {
+                    medicineToDelete?.let { medicineViewModel.deleteReminder(it.id_reminder) }
+                    medicineToDelete = null
+                },
+                content = {
+                    Text(
+                        text = "¿Estás seguro de que quieres eliminar el recordatorio para tu " +
+                                "medicación \"${medicineToDelete?.title}\"? Esta acción no se puede deshacer."
+                    )
+                },
+            )
+
+        if (isRefreshing) {
+            SplashScreen()
+        }
     }
 }
