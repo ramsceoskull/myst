47d633d5c876c08bce32ff6bf1c18963dd2f624c
Ramses Anceno <ramses.social0@gmail.com>
2026-05-17 01:26:36 -0600

    New version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.tenko.app"
         minSdk = 30
         targetSdk = 36
-        versionCode = 24
-        versionName = "1.4.2"
+        versionCode = 25
+        versionName = "1.4.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
