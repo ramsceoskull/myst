8daa4c94045ca7daf24cfc4c0295183683e52825
Ramses Anceno <ramses.social0@gmail.com>
2026-05-17 08:17:48 -0600

    Laboratory variable data class

---
+++ app/src/main/java/com/tenko/app/data/model/LaboratoryVariable.kt
@@ -0,0 +1,8 @@
+package com.tenko.app.data.model
+
+data class LaboratoryVariable(
+    var parameter: String = "",
+    var value: String = "",
+    var unit: String = "",
+    var expanded: Boolean = true
+)
