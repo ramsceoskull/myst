8dd38ec7807d1043d26a614a2ef666239fed32a7
Ramses Anceno <ramses.social0@gmail.com>
2026-05-17 01:27:33 -0600

    Variables unused deleted

--- app/src/main/java/com/tenko/app/ui/components/AppTopBar.kt
+++ app/src/main/java/com/tenko/app/ui/components/AppTopBar.kt
@@ -1,20 +1,13 @@
 package com.tenko.app.ui.components
 
-import androidx.compose.animation.AnimatedContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ButtonDefaults
@@ -26,57 +19,30 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableLongStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.net.toUri
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.tenko.app.R
-import com.tenko.app.data.model.Quote
 import com.tenko.app.data.view.AuthViewModel
-import com.tenko.app.data.view.NotificationViewModel
 import com.tenko.app.navigation.AppScreens
 import com.tenko.app.ui.theme.AntiFlashWhite
-import com.tenko.app.ui.theme.Monserrat
-import com.tenko.app.ui.theme.PompAndPower
 import com.tenko.app.ui.theme.StarsLove
 import com.tenko.app.ui.theme.SweetGrey
 import com.tenko.app.ui.theme.Tekhelet
 import com.tenko.app.ui.theme.White
-import kotlinx.coroutines.delay
-
-val quotes = listOf(
-    Quote(
-        id = 0,
-        quote = "Una mujer con fuerza interior no solo enfrenta las tormentas, las usa para crecer y brillar aún más",
-        author = "Autor desconocido"
-    ),
-    Quote(
-        id = 1,
-        quote = "Tu bienestar es tu mayor poder",
-        author = "Myst"
-    ),
-    Quote(
-        id = 2,
-        quote = "Escuchar tu cuerpo también es amor propio",
-        author = "Myst"
-    )
-)
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -195,8 +161,6 @@ fun AppTopBar(
 fun AppTopBar(
     title: String,
     navController: NavController,
-    scrollBehavior: TopAppBarScrollBehavior,
-    notificationViewModel: NotificationViewModel,
     authViewModel: AuthViewModel = viewModel(),
     actions: (() -> Unit)? = null
 ) {
@@ -210,8 +174,6 @@ fun AppTopBar(
             authViewModel.getUser(navController)
         }
     }
-
-    val hasUnread = notificationViewModel.hasUnread
 
     Surface(
         shadowElevation = 4.dp,
@@ -241,127 +203,11 @@ fun AppTopBar(
                     content = { ProfilePicture(user?.picture?.toUri(), 40.dp, false) }
                 )
             },
-            actions = {
-                actions?.let {
-                    IconButton(onClick = actions) {
-                        if (hasUnread) {
-                            Icon(
-                                tint = null,
-                                contentDescription = "Bandeja de entrada",
-                                painter = painterResource(R.drawable.bell_new_notification),
-                                modifier = Modifier.size(42.dp)
-                            )
-                        } else {
-                            Icon(
-                                tint = null,
-                                contentDescription = "Bandeja de entrada",
-                                painter = painterResource(R.drawable.bell_no_notification),
-                                modifier = Modifier.size(42.dp)
-                            )
-                        }
-                    }
-                }
-            },
-            colors = TopAppBarDefaults.topAppBarColors(
-                containerColor = White,
-                scrolledContainerColor = White
-            ),
-
-            scrollBehavior = scrollBehavior,
+            colors = TopAppBarDefaults.topAppBarColors(containerColor = White),
             modifier = Modifier
                 .fillMaxWidth()
                 .background(White)
                 .padding(horizontal = 8.dp)
         )
-    }
-}
-
-@Composable
-fun WelcomeSection() {
-    var index by remember { mutableIntStateOf(0) }
-
-    LaunchedEffect(Unit) {
-        while (true) {
-            delay(8000)
-            index = (index + 1) % quotes.size
-        }
-    }
-
-    Column(
-        modifier = Modifier
-            .height(IntrinsicSize.Min)
-            .padding(start = 4.dp, end = 22.dp, bottom = 8.dp)
-    ) {
-        Text(
-            text = "Hello,",
-            fontSize = 20.sp,
-            fontFamily = Monserrat,
-            fontWeight = FontWeight.Medium,
-            color = SweetGrey
-        )
-        Text(
-            text = "Alessandra Wins",
-            fontSize = 32.sp,
-            fontFamily = StarsLove,
-            fontWeight = FontWeight.SemiBold,
-            color = Tekhelet
-        )
-
-        DotsIndicator(
-            total = quotes.size,
-            selectedIndex = index
-        )
-
-        Spacer(modifier = Modifier.height(12.dp))
-
-        AnimatedContent /*Crossfade*/(
-            targetState = index,
-            label = ""
-        ) { i ->
-            Column {
-                Text(
-                    color = SweetGrey,
-                    text = "\"${quotes[i].quote}\"",
-                    fontSize = 14.sp,
-                    fontFamily = Monserrat,
-                    lineHeight = 20.sp,
-                    fontWeight = FontWeight.Medium,
-                    textAlign = TextAlign.Justify
-                )
-
-                Text(
-                    color = PompAndPower,
-                    text = "- ${quotes[i].author}",
-                    fontSize = 12.sp,
-                    fontFamily = Monserrat,
-                    fontWeight = FontWeight.Medium,
-                )
-            }
-        }
-    }
-}
-
-@Composable
-fun DotsIndicator(
-    total: Int,
-    selectedIndex: Int
-) {
-    Row {
-        repeat(total) { i ->
-            Box(
-                modifier = Modifier
-                    .padding(end = 4.dp)
-                    .size(
-                        if (i == selectedIndex) 8.dp else 6.dp
-                    )
-                    .clip(CircleShape)
-                    .background(
-                        if (i == selectedIndex)
-                            Tekhelet
-                        else
-                            SweetGrey
-                    )
-            )
-        }
     }
 }
