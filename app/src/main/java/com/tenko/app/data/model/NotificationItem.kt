d4d0575fb3ce1f3791f2e2cabff825d6e19a0b2f
Ramses Anceno <ramses.social0@gmail.com>
2026-05-17 07:48:42 -0600

    No longer using notifications

--- app/src/main/java/com/tenko/app/data/model/NotificationItem.kt
+++
@@ -1,8 +0,0 @@
-package com.tenko.app.data.model
-
-data class NotificationItem(
-    val id: Int,
-    val title: String = "New Notification",
-    val message: String = "You have a new notification.",
-    val isRead: Boolean = false
-)
