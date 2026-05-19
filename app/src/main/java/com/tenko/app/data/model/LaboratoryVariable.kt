0c08af0301b8ea1d95a389f135b023c00b0db7f0
Ramses Anceno <ramses.social0@gmail.com>
2026-05-18 18:39:05 -0600

    New attributes to handle individual log errors

--- app/src/main/java/com/tenko/app/data/model/LaboratoryVariable.kt
+++ app/src/main/java/com/tenko/app/data/model/LaboratoryVariable.kt
@@ -4,5 +4,10 @@ data class LaboratoryVariable(
     var parameter: String = "",
     var value: String = "",
     var unit: String = "",
-    var expanded: Boolean = true
+    var expanded: Boolean = false,
+    var hasError: Boolean = false,
+
+    var parameterError: String? = null,
+    var valueError: String? = null,
+    var unitError: String? = null
 )
